20ae5a15b0bccf0578de7ae74c2aff5f7cacdc75
John Doe <thiagoruis.mail@gmail.com>
2020-01-28 00:12:07 -0300

    fechada funcionalidade rudimentar de traduão

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,49 +12,70 @@ import 'moment/locale/pt-br';
 export class AppComponent  {
   polledCurrency: any;
   ultimaAtualizacao: any;
+  timer: any;
   currencyFrom: string;
   currencyTo: string;
   language = 'pt-br';
   amount: number;
+  prefixes: {'BRL': 'R$', 'USD': 'U$', 'EUR': '€', 'BTC': 'BTC'};
 
   constructor(private http: HttpClient) { }
 
   currencyLookup() {
-    const timerRequest = timer(0, 5000);
+    if(this.timer) {
+      this.timer.unsubscribe();
+    }
+    const timerRequest = timer(0, 15000);
 
-    timerRequest.subscribe(() => {
+    this.timer = timerRequest.subscribe(() => {
       this.jiraya();
     });
   }
 
   jiraya() {
-    debugger;
-    if (false) {
+    if(this.amount === 0 ) {
+      this.polledCurrency = 0;
+      this.timer.unsubscribe();
+      return null;
+    }
+
+    if (this.currencyTo === this.currencyFrom) {
+      this.polledCurrency = this.amount;
+      this.timer.unsubscribe();
+      return null;
+    }
+
+    if (this.currencyFrom === 'BTN') {
       this.http.get('https://blockchain.info/ticker').subscribe(
-        (data: any) => this.polledCurrency = data.USD.last
+        (data: any) => this.polledCurrency = this.amount * data[this.currencyTo].last
+      );
+    } else if(this.currencyTo === 'BTN') {
+      this.http.get('https://blockchain.info/ticker').subscribe(
+        (data: any) => this.polledCurrency = this.amount / data[this.currencyFrom].last
+      );
+    } else {
+      this.http.get(`https://api.exchangerate-api.com/v4/latest/${this.currencyFrom}`).subscribe(
+        (data: any) => this.polledCurrency = this.amount * data.rates[this.currencyTo]
       );
     }
 
-    if (true) {
-      this.http.get('https://api.exchangerate-api.com/v4/latest/USD').subscribe(
-        (data: any) => this.polledCurrency = data.rates.AUD
-      );
-    }
 
     const now = moment();
     this.ultimaAtualizacao = now.format('HH:MM:ss'); // en
-    console.log(this.currencyTo);
-    moment.locale('pt-BR');
-    console.log(now.locale()); // pt-BR
-    console.log('PAsei');
   }
 
   setCurrencyFrom(value) {
     this.currencyFrom = value;
+    if(this.timer) {
+      this.timer.unsubscribe();
+    }
   }
 
   setCurrencyTo(value) {
     this.currencyTo = value;
+    if(this.timer) {
+      this.timer.unsubscribe();
+    }
   }
 
   setLanguage(language) {
