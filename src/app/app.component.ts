7fe824e70e7408aa8d00f81b7f8080091bc5d0c9
John Doe <thiagoruis.mail@gmail.com>
2020-01-27 21:07:22 -0300

    Finalizados todos os campos de entrada e funcoes rudimentares de consulta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { timer } from 'rxjs';
+import * as moment from 'moment';
+import 'moment/locale/pt-br';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'currency-converter';
+export class AppComponent  {
+  polledCurrency: any;
+  ultimaAtualizacao: any;
+  currencyFrom: string;
+  currencyTo: string;
+  language = 'pt-br';
+  amount: number;
+
+  constructor(private http: HttpClient) { }
+
+  currencyLookup() {
+    const timerRequest = timer(0, 5000);
+
+    timerRequest.subscribe(() => {
+      this.jiraya();
+    });
+  }
+
+  jiraya() {
+    debugger;
+    if (false) {
+      this.http.get('https://blockchain.info/ticker').subscribe(
+        (data: any) => this.polledCurrency = data.USD.last
+      );
+    }
+
+    if (true) {
+      this.http.get('https://api.exchangerate-api.com/v4/latest/USD').subscribe(
+        (data: any) => this.polledCurrency = data.rates.AUD
+      );
+    }
+
+    const now = moment();
+    this.ultimaAtualizacao = now.format('HH:MM:ss'); // en
+    console.log(this.currencyTo);
+    moment.locale('pt-BR');
+    console.log(now.locale()); // pt-BR
+    console.log('PAsei');
+  }
+
+  setCurrencyFrom(value) {
+    this.currencyFrom = value;
+  }
+
+  setCurrencyTo(value) {
+    this.currencyTo = value;
+  }
+
+  setLanguage(language) {
+    this.language = language;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -17,6 +20,7 @@ import {MatButtonModule} from '@angular/material/button';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
@@ -24,6 +28,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatButtonToggleModule,
     MatInputModule,
     MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
